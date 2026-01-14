6373b6938accc1e801fc9ecf9ecd949260b02118
e-h-dev <ebaal8226@gmail.com>
2026-01-14 01:08:52 +0000

    Update return toggle to dark mode

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let toggleDark = document.getElementById('theme-toggle-drk');
 let toggleLight = document.getElementById('theme-toggle-light');
 let main = document.getElementById('main');
 
-function toggle() {
+function toggleLgt() {
     main.style.cssText = `
         background-color: #fff;
         color: rgb(2, 2, 83);
@@ -10,14 +10,31 @@ function toggle() {
 
     toggleLight.style.cssText = `
         display: block;
-        color: rgb(1, 1, 23);
+        color: rgb(2, 2, 83);
     `;
 
     toggleDark.style.display = 'none';
 }
 
+function toggleDrk() {
+    main.style.cssText = `
+        background-color: rgb(1, 1, 23);
+        color: #fff;
+    `;
 
-toggleDark.addEventListener('click', () => toggle())
+    toggleDark.style.cssText = `
+        display: block;
+        color: #fff;
+    `;
+
+    toggleLight.style.display = 'none';
+}
+
+
+toggleLight.addEventListener('click', () => toggleDrk())
+
+
+toggleDark.addEventListener('click', () => toggleLgt())
 
 
 let form = document.getElementById('form')
